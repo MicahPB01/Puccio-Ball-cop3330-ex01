8b65a22470d98b9ce22a20627914704c18c334be
MicahPB01 <micahpuccioball@gmail.com>
2021-05-29 16:15:34 -0400

    Initial commit

---
+++ src/main/java/oop/example/App.java
@@ -0,0 +1,25 @@
+package oop.example;
+
+//Exercise 01
+import java.util.Scanner;
+public class App 
+{
+    public static void main( String[] args )
+
+    {
+        Scanner getUserName = new Scanner(System.in);
+
+        System.out.print( "What is your name? ");
+
+        String userName = getUserName.nextLine();
+
+        System.out.printf("Hello %s, nice to meet you!", userName);
+
+
+
+
+
+    }
+
+
+}
